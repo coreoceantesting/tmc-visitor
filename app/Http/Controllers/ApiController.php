262b584e0879d8d44bb082fac10bafa19c652e9e
coreoceantesting <coreoceantesting@gmail.com>
2023-10-11 13:27:30 +0530

    login logout api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Token;
+use Illuminate\Support\Str;
 
 class ApiController extends Controller
 {
     //
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['username' => $request->username, 'password' => $request->password])) {
+            $user = Auth::user();
+            $token = explode('|', $user->createToken('AuthToken')->plainTextToken, 2);
+            $user['token']= $token[1];
+            return response()->json(['success'=>'true','data' => $user,'message'=>'Login Success'], 200);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', function (Request $request) {
-    $credentials = $request->only('username', 'password');
+// Route::post('/login', function (Request $request) {
+//     $credentials = $request->only('username', 'password');
 
-    if (Auth::attempt($credentials)) {
-        $user = Auth::user();
-        return response()->json(['user' => $user], 200);
-    }
+//     if (Auth::attempt($credentials)) {
+//         $user = Auth::user();
+//         return response()->json(['success'=>'true','data' => $user,'message'=>'Action Successful'], 200);
+//     }
 
-    return response()->json(['error' => 'Unauthorized'], 401);
-});
+//     return response()->json(['error' => 'Unauthorized'], 401);
+// });
+
+Route::post('/login', 'App\Http\Controllers\ApiController@login');
+Route::middleware('auth:sanctum')->post('/logout', 'App\Http\Controllers\ApiController@logout');
