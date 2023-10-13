8735b4f5d8bb45d0f62174c24dc946d3818a11c1
coreoceantesting <coreoceantesting@gmail.com>
2023-10-13 18:59:39 +0530

    add datatable

--- resources/views/Masters/departmentlist.blade.php
+++ resources/views/Masters/departmentlist.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.master')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+
 
 <style>
     .card-body {
@@ -44,8 +46,8 @@ li {
             </div>
         </div>
     </div>
-    <div class="row">
-        <table class="table table-bordered">
+    <div class="justify-content-center">
+        <table id="departmentTable" class="table table-bordered">
             <thead>
                 <tr>
                     <th scope="col">Id</th>
@@ -96,7 +98,17 @@ li {
     </div>
 </div>
 
-@endsection
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js" defer></script>
+
+<script>
+    $(document).ready(function() {
+        $('#departmentTable').DataTable({
+            "searching": false,
+            "paging" : true
+        });
+    });
+</script>
+
+@endsection
+
